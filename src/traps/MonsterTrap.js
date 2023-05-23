ba909a01350a7c0cacbc4b776352e7a8f629ee18
Maeva SANCIO <maeva.sancio@mail-esd.com>
2023-05-23 09:58:31 +0200

    add MonsterTrap

--- src/traps/MonsterTrap.js
+++ src/traps/MonsterTrap.js
@@ -1,7 +1,7 @@
 class MonsterTrap extends Trap {
   scene;
-  platformSprite;
-  discSprite;
+  piques;
+  ground;
 
   x;
   y;
@@ -13,17 +13,17 @@ class MonsterTrap extends Trap {
     this.x = x;
     this.y = y;
 
-    this.piques = scene.physics.add.image(x, y + 8, "trap-monster-disc");
+    this.piques = scene.physics.add.image(x, y + 8, "trap-monster-piques");
     this.piques.setScale(1);
     this.piques.body.setAllowGravity(false);
 
-    this.ground = scene.physics.add.image(x, y, "trap-monster-platform");
+    this.ground = scene.physics.add.image(x, y, "trap-monster-ground");
     this.ground.setScale(1);
     this.ground.body.setAllowGravity(false);
     this.ground.setImmovable(true);
     this.scene.physics.add.collider(this.ground, this.scene.player.sprite);
 
-    this.piques.setVelocityY(-10);
+    this.piques.setVelocityY(-50);
   }
 
   update() {
