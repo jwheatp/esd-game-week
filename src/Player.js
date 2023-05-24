8a31b74a3481d418569b3a911cb09b53aa1f33cd
celine0299 <celine.turlot17@gmail.com>
2023-05-24 09:22:27 +0200

    add

--- src/Player.js
+++ src/Player.js
@@ -1,85 +1,74 @@
 class Player {
-  scene
-  sprite
-  speed = 200
-  jump = 400
-  canMove = true
-  isJumping = false
+  scene;
+  sprite;
+  speed = 200;
+  jump = 400;
+  canMove = true;
+  isJumping = false;
 
-  lastSpeedX = 0
-  lastSpeedY = 0
-
-
+  lastSpeedX = 0;
+  lastSpeedY = 0;
 
   constructor(scene, x, y) {
-    this.scene = scene
+    this.scene = scene;
 
     this.sprite = scene.physics.add.image(x, y, "player");
 
-    this.sprite.body.setMass(1000)
+    this.sprite.body.setMass(1000);
   }
 
   update() {
     if (!this.canMove) {
-      this.sprite.setVelocityX(0)
-      return
+      this.sprite.setVelocityX(0);
+      return;
     }
 
     // saut
     if (!this.isJumping && this.scene.inputs.up.isDown) {
-      this.isJumping = true
+      this.isJumping = true;
       // je mets une vitesse X à 200
       this.sprite.setVelocityY(-this.jump);
-      this.lastSpeedY = -this.jump
+      this.lastSpeedY = -this.jump;
     }
-
-
 
     // déplacement horizontal
     if (this.scene.inputs.right.isDown) {
       // je mets une vitesse X à 200
       this.sprite.setVelocityX(this.speed);
-      this.lastSpeedX = this.speed
-    }
-    else if (this.scene.inputs.left.isDown) {
+      this.lastSpeedX = this.speed;
+    } else if (this.scene.inputs.left.isDown) {
       // je mets une vitesse X à 200
       this.sprite.setVelocityX(-this.speed);
-      this.lastSpeedX = -this.speed
-    }
-
-    else if (this.scene.inputs.down.isDown) {
+      this.lastSpeedX = -this.speed;
+    } else if (this.scene.inputs.down.isDown) {
       // je mets une vitesse X à 200
       this.sprite.setVelocityY(this.speed);
-    }
-    else {
+    } else {
       // sinon, je remets la vitesse à 0
       this.sprite.setVelocityX(0);
-      this.lastSpeedX = 0
+      this.lastSpeedX = 0;
     }
 
     if (Math.abs(this.sprite.body.velocity.y) === 0) {
-      this.isJumping = false
+      this.isJumping = false;
     }
-
-
   }
 
-
   freeze() {
-    this.canMove = false
-    this.sprite.body.setAllowGravity(false)
+    this.canMove = false;
+    this.sprite.body.setAllowGravity(false);
   }
 
   unfreeze() {
-    this.canMove = true
-    this.sprite.body.setAllowGravity(true)
+    this.canMove = true;
+    this.sprite.body.setAllowGravity(true);
   }
 
   die() {
-    console.log('le joueur est mort !')
+    console.log("le joueur est mort !");
 
-    this.sprite.setScale(0.5, 0.1)
-    this.canMove = false
+    this.sprite.setScale(0.5, 0.1);
+    this.canMove = false;
 
     this.canMove = false;
 
@@ -89,7 +78,7 @@ class Player {
     let blinkCount = 0;
 
     const blinkIntervalId = setInterval(() => {
-      this.sprite.alpha = (this.sprite.alpha === 1) ? 0.2 : 1;
+      this.sprite.alpha = this.sprite.alpha === 1 ? 0.2 : 1;
 
       blinkCount++;
 
