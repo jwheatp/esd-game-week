3038e96107ec628981edf082d78baaedf201f07e
Maeva SANCIO <maeva.sancio@mail-esd.com>
2023-05-25 16:37:31 +0200

    scene test

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const config = {
     },
   },
   audio: {
-    disableWebAudio: true
+    disableWebAudio: true,
   },
-  scene: Scene5,
+  scene: Scene2,
 };
 
 const game = new Phaser.Game(config);
