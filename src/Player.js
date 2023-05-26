3ee910d71c2cf479c28a58f7eb9b1dc3f2ac7738
Sixte Morio de l'Isle <moriodelislesixte@gmail.com>
2023-05-26 12:01:29 +0200

    je tue le projet (sixte antoine - fin anim )

--- src/Player.js
+++ src/Player.js
@@ -6,6 +6,8 @@ class Player {
   canMove = true;
   isJumping = false;
 
+  skinNumber = 1;
+
   lastSpeedX = 0;
   lastSpeedY = 0;
   scoreText;
@@ -13,7 +15,7 @@ class Player {
 
   hasWon = false;
   inputPayload = {
-    
+
   };
 
   inputQueue = []
@@ -50,27 +52,45 @@ class Player {
     //   this.canMove = false
     // });
 
-    /*tests animations*/
+    /*animations*/
     this.scene.anims.create({
-      key: "anim-player-run",
-      frames: [{ key: "player-run" }, { key: "player-walk" }],
+      key: 'anim-player-' + this.skinNumber + '-run',
+      frames: [{ key: "player-" + this.skinNumber + "-run" }, { key: "player-" + this.skinNumber + "-walk" }],
       frameRate: 7,
       repeat: -1,
     });
 
     this.scene.anims.create({
-      key: "anim-player-idl",
-      frames: [{ key: "player-idl" }],
+      key: 'anim-player-' + this.skinNumber + '-idl',
+      frames: [{ key: "player-" + this.skinNumber + "-idl" }],
       frameRate: 7,
       repeat: -1,
     });
 
     this.scene.anims.create({
-      key: "anim-player-jump",
-      frames: [{ key: "player-jump" }],
+      key: 'anim-player-' + this.skinNumber + '-jump',
+      frames: [{ key: "player-" + this.skinNumber + "-jump" }],
       frameRate: 7,
       repeat: -1,
     });
+    this.sprite.body.setMass(1000);
+
+    this.scene.anims.create({
+      key: 'anim-player-' + this.skinNumber + '-death',
+      frames: [{ key: "player-" + this.skinNumber + "-death" }],
+      frameRate: 7,
+      repeat: -1
+    });
+
+    this.scene.anims.create({
+      key: 'anim-player-' + this.skinNumber + '-victory',
+      frames: [{ key: "player-" + this.skinNumber + "-move1" }, { key: "player-" + this.skinNumber + "-move2" }, {
+        key: "player-" + this.skinNumber + "-move3"
+      }, { key: "player-" + this.skinNumber + "-move4" }],
+      frameRate: 7,
+      repeat: -1
+    })
+
     this.sprite.body.setMass(1000);
 
     // Platform.addCollider(this.sprite)
@@ -79,7 +99,6 @@ class Player {
     // this.scene.physics.add.overlap(this.scene.endPoint, this.sprite, () => {
     //   this.winRound();
     // });
-    this.sprite.body.setMass(1000)
 
   }
 
@@ -90,7 +109,7 @@ class Player {
     this.sprite.setScale(0.5);
     this.canMove = true;
 
-    this.sprite.play("anim-player-idl", true);
+    this.sprite.play("anim-player-" + this.skinNumber + "-idl", true);
   }
 
   //score
@@ -132,10 +151,10 @@ class Player {
       // this.scene.sound.play("jump");
 
       // je mets une vitesse X à 200
-      this.sprite.body.setVelocityY(-2*this.speed)
+      this.sprite.body.setVelocityY(-2 * this.speed)
 
       this.lastSpeedY = -this.jump;
-      this.sprite.play("anim-player-jump", true);
+      this.sprite.play("anim-player-" + this.skinNumber + "-jump", true);
       this.inputPayload.animation = "anim-player-jump"
     }
 
@@ -143,25 +162,40 @@ class Player {
     if (this.scene.inputs.right.isDown) {
       // je mets une vitesse X à 200
       this.sprite.body.setVelocityX(this.speed)
-      //this.lastSpeedX = this.speed;
-      this.sprite.play('anim-player-run', true);
       this.inputPayload.animation = "anim-player-run"
       this.lastSpeedX = this.speed;
+      this.sprite.flipX = false
+      //lancement de l'animation
+      if (!this.isJumping) {
+        this.sprite.play('anim-player-' + this.skinNumber + '-run', true);
+      }
 
-    } else if (this.scene.inputs.left.isDown) {
+    }
+    else if (this.scene.inputs.left.isDown) {
       // je mets une vitesse X à 200
-
       this.sprite.body.setVelocityX(-this.speed)
+      //launch animation
+      if (!this.isJumping) {
+        this.sprite.play('anim-player-' + this.skinNumber + '-run', true);
+        //reverse player
+        this.sprite.flipX = true
+      }
+
     } else {
       // sinon, je remets la vitesse à 0
       this.sprite.body.setVelocityX(0)
 
-      if(!this.isJumping) {
-        this.sprite.play('anim-player-idl', true);
+      if (!this.isJumping) {
+        this.sprite.play('anim-player-' + this.skinNumber + '-idl', true);
         this.inputPayload.animation = "anim-player-idl"
 
       }
       this.lastSpeedX = 0;
+
+      if (!this.isJumping || this.isDie) {
+        this.sprite.play('anim-player-' + this.skinNumber + '-idl', true);
+
+      }
     }
 
 
@@ -169,6 +203,11 @@ class Player {
     // this.inputPayload.up = true
     if (Math.abs(this.sprite.body.velocity.y) === 0) {
       this.isJumping = false;
+    }
+
+    //if player win
+    if (this.nPoint) {
+      this.sprite.play('anim-player-' + this.skinNumber + '-victory', true)
     }
   }
 
@@ -182,14 +221,15 @@ class Player {
     this.sprite.body.setAllowGravity(true);
   }
 
-  fall() {}
+  fall() { }
 
   die() {
     console.log("le joueur est mort !");
 
     this.isDead = true;
 
-    this.sprite.setScale(0.5, 0.1);
+    this.sprite.play('anim-player-' + this.skinNumber + '-death', true);
+
     this.canMove = false;
     this.isDead = true;
 
--- src/traps/BlackHoleTrap.js
+++ src/traps/BlackHoleTrap.js
@@ -1,21 +1,16 @@
 class BlackHoleTrap extends Trap {
-  scene
-  x
-  y
-  blackHoleSprite
-
-
+  scene;
+  x;
+  y;
+  blackHoleSprite;
   constructor(scene, x, y) {
-    super()
-
-    this.scene = scene
-    this.x = x
-    this.y = y
-
-    this.blackHoleSprite = scene.physics.add.image(x, y, "trap-blackHole");
+    super();
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+    this.blackHoleSprite = scene.physics.add.image(x, y, "trap - blackHole");
     this.blackHoleSprite.setScale(0.2);
     this.blackHoleSprite.body.setAllowGravity(false);
-
     this.scene.tweens.add({
       targets: this.blackHoleSprite,
       rotation: 360,
@@ -23,52 +18,108 @@ class BlackHoleTrap extends Trap {
       repeat: 10000,
       ease: "Linear",
     });
+    this.hbBlackHole = new HbBlackHole(this.scene, this.x, this.y);
+    this.physics.add.overlap(
+      this.scene.player.sprite,
+      this.hbBlackHole.sprite,
+      () => {
+        // Faire disparaître le joueur
+        this.scene.player.die();
+        // Autres actions à effectuer en cas de collision avec hbBlackHole...
+      }
+    );
   }
-
   update() {
-    //trap BlackHole attraction 
-    if (this.scene.player.sprite.body.x >= this.blackHoleSprite.body.x - 100 &&
-      this.scene.player.sprite.body.y >= this.blackHoleSprite.body.y - 100 && this.scene.player.sprite.body.x <= this.blackHoleSprite.body.x + 100 && this.scene.player.sprite.body.y <= this.blackHoleSprite.body.y + 100) {
-
-      const playerVelocityX = this.scene.player.lastSpeedX
-      const playerVelocityY = this.scene.player.lastSpeedY
-      const attraction = 30
-
-      const shrink = Math.min(Math.abs(this.scene.player.sprite.body.x - this.blackHoleSprite.body.x) / 100, 1)
-
+    //trap BlackHole attraction
+    if (
+      this.scene.player.sprite.body.x >= this.blackHoleSprite.body.x - 100 &&
+      this.scene.player.sprite.body.y >= this.blackHoleSprite.body.y - 100 &&
+      this.scene.player.sprite.body.x <= this.blackHoleSprite.body.x + 100 &&
+      this.scene.player.sprite.body.y <= this.blackHoleSprite.body.y + 100
+    ) {
+      const playerVelocityX = this.scene.player.lastSpeedX;
+      const playerVelocityY = this.scene.player.lastSpeedY;
+      const attraction = 30;
+      const shrink = Math.min(
+        Math.abs(
+          this.scene.player.sprite.body.x - this.blackHoleSprite.body.x
+        ) / 100,
+        1
+      );
       if (this.scene.player.sprite.body.x < this.blackHoleSprite.body.x) {
-        this.scene.player.sprite.setVelocityX(attraction + playerVelocityX)
-        this.scene.player.sprite.setScale(shrink)
+        this.scene.player.sprite.setVelocityX(attraction + playerVelocityX);
+        this.scene.player.sprite.setScale(shrink);
         this.scene.player.sprite.rotation += 0.5;
-
-      }
-      else if (this.scene.player.sprite.body.x > this.blackHoleSprite.body.x) {
-        this.scene.player.sprite.setVelocityX(-attraction + playerVelocityX)
-        this.scene.player.sprite.setScale(shrink)
+      } else if (
+        this.scene.player.sprite.body.x > this.blackHoleSprite.body.x
+      ) {
+        this.scene.player.sprite.setVelocityX(-attraction + playerVelocityX);
+        this.scene.player.sprite.setScale(shrink);
         this.scene.player.sprite.rotation += 0.5;
       }
-
       if (this.scene.player.sprite.body.y < this.blackHoleSprite.body.y) {
-        this.scene.player.sprite.setVelocityY(attraction + playerVelocityY)
-        this.scene.player.sprite.setScale(shrink)
+        this.scene.player.sprite.setVelocityY(attraction + playerVelocityY);
+        this.scene.player.sprite.setScale(shrink);
+        this.scene.player.sprite.rotation += 0.5;
+      } else if (
+        this.scene.player.sprite.body.y > this.blackHoleSprite.body.y
+      ) {
+        this.scene.player.sprite.setVelocityY(-attraction + playerVelocityY);
+        this.scene.player.sprite.setScale(shrink);
         this.scene.player.sprite.rotation += 0.5;
       }
-      else if (this.scene.player.sprite.body.y > this.blackHoleSprite.body.y) {
-        this.scene.player.sprite.setVelocityY(-attraction + playerVelocityY)
-        this.scene.player.sprite.setScale(shrink)
-        this.scene.player.sprite.rotation += 0.5;
-      }
-
       /*
       const signX = Math.sign(this.scene.player.sprite.body.velocity.x)
       this.scene.player.sprite.setVelocityX(-signX * 50);
       */
-    }
-    else {
-      this.scene.player.sprite.setScale(1)
-
+    } else {
+      this.scene.player.sprite.setScale(1);
       this.scene.player.sprite.rotation = 0;
     }
-
+  }
+  setX(x) {
+    this.blackHoleSprite.x = x;
+    this.sprite.x = x;
+    this.x = x;
+  }
+  setY(y) {
+    this.blackHoleSprite.y = y;
+    this.sprite.y = y;
+    this.y = y;
   }
 }
+class HbBlackHole {
+  scene;
+  x;
+  y;
+  sprite;
+  constructor(scene, x, y) {
+    super();
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+    this.HbBlackHoleSprite = scene.physics.add.image(x, y, "hb - trap - blackHole");
+    this.HbBlackHoleSprite.setScale(0.4);
+    this.HbBlackHoleSprite.body.setAllowGravity(false);
+  }
+  update() { }
+  setX(x) {
+    this.HbBlackHoleSprite.x = x;
+    this.sprite.x = x;
+    this.x = x;
+  }
+  setY(y) {
+    this.HbBlackHoleSprite.y = y;
+    this.sprite.y = y;
+    this.y = y;
+  }
+}
+
+
+
+
+
+
+
+
+
